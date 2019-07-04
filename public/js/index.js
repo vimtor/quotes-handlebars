8f68ef02c88917c600470d87cd5d47de095d4e51
Víctor Navarro González <vn4varro@gmail.com>
2019-07-04 15:49:15 +0200

    Finished HTML structure

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,20 @@
-console.log('Javascript is working!');
+const $modalButtons = document.querySelectorAll('.modal-button');
+const $modalCloseButtons = document.querySelectorAll('.modal-close');
+
+$modalButtons.forEach($button => {
+    $button.addEventListener('click', () => openModal($button.dataset.target))
+});
+
+$modalCloseButtons.forEach($button => {
+    $button.addEventListener('click', () => closeModal($button.dataset.target))
+});
+
+function openModal(modalID) {
+    const $modal = document.getElementById(modalID);
+    $modal.classList.add('is-active');
+}
+
+function closeModal(modalID) {
+    const $modal = document.getElementById(modalID);
+    $modal.classList.remove('is-active');
+}
