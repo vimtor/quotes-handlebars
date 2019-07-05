04f7717f584e21358a4f9dde6de217e5d5509e32
Víctor Navarro González <vn4varro@gmail.com>
2019-07-05 21:32:37 +0200

    User model

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: Date.now
+    },
+});
+
+mongoose.model('User', userSchema);
