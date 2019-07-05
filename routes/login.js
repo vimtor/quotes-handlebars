03905b173f8aa1faf1880611ed72ed8501ed21b6
Víctor Navarro González <vn4varro@gmail.com>
2019-07-05 13:41:34 +0200

    Login and register unique page

--- routes/login.js
+++ routes/login.js
@@ -2,23 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index.hbs', {
-        quotes: [
-            {
-                text: 'There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.',
-                author: 'Jeff Atwood'
-            },
-            {
-                text: 'My name is Ralph!',
-                author: 'Ralph Wiggum'
-            },
-            {
-                text: 'Be water, my friend.',
-                author: 'Bruce Lee'
-            }
-        ],
-        showLoginModal: true
-    });
+    res.render('login.hbs');
 });
 
 module.exports = router;
