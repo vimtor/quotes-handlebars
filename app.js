44510d4b8f6e0548226b4f2dc086d0df9b45d6b6
Víctor Navarro González <vn4varro@gmail.com>
2019-07-06 20:45:30 +0200

    Globals separate file

--- app.js
+++ app.js
@@ -20,13 +20,7 @@ mongoose.connect(keys.mongoURI, {useNewUrlParser: true})
 require('./config/passport')(passport);
 
 // Global Variables
-app.use((req, res, next) => {
-    res.locals.success_msg = req.flash('success');
-    res.locals.error_msg = req.flash('error');
-    res.locals.user = req.user || null;
-
-    next();
-});
+require('./config/globals')(app);
 
 // Basic Routing
 require('./routes')(app);
---
+++ config/globals.js
@@ -0,0 +1,9 @@
+module.exports = function (app) {
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash('success');
+        res.locals.error_msg = req.flash('error');
+        res.locals.user = req.user || null;
+
+        next();
+    });
+};
