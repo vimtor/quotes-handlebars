f35eb0669c0aac6750aa0aee1581d9ef86d1f435
Víctor Navarro González <vn4varro@gmail.com>
2019-07-07 23:03:57 +0200

    Async await routes

--- routes/quotes.js
+++ routes/quotes.js
@@ -6,24 +6,32 @@ const router = express.Router();
 require('../models/Quote');
 const Quote = mongoose.model('Quote');
 
-router.get('/public', (req, res) => {
-    Quote.find({visible: true}).sort({'likes': 'desc'})
-        .then(quotes => res.render('quotes/public.hbs', {quotes}))
-        .catch(err => console.log(err));
+router.get('/public', async (req, res) => {
+    try {
+        const quotes = await Quote.find({visible: true}).sort({'likes': 'desc'});
+        res.render('quotes/public.hbs', {quotes});
+    }
+    catch (err) {
+        console.log(err);
+    }
 });
 
-router.get('/private', ensureAutheticated, (req, res) => {
-    Quote.find({user: req.user.id})
-        .then(quotes => {
-            res.render('quotes/private.hbs', {quotes});
-        })
+router.get('/private', ensureAutheticated, async (req, res) => {
+    try {
+        const quotes = await Quote.find({user: req.user.id});
+        res.render('quotes/private.hbs', {quotes});
+    }
+    catch (err) {
+        console.log(err);
+        res.redirect('/quotes/public');
+    }
 });
 
 router.get('/add', ensureAutheticated, (req, res) => {
     res.render('quotes/add.hbs');
 });
 
-router.post('/', ensureAutheticated, (req, res) => {
+router.post('/', ensureAutheticated, async (req, res) => {
     const {author, quote, visible} = req.body;
 
     if (!quote) {
@@ -31,110 +39,88 @@ router.post('/', ensureAutheticated, (req, res) => {
         return res.redirect('/quotes/add');
     }
 
-    const newQuote = {
-        quote,
-        author: author !== '' ? author : undefined,
-        user: req.user.id,
-        visible: !!visible
-    };
+    try {
+        await new Quote({
+            quote,
+            author: author !== '' ? author : undefined,
+            user: req.user.id,
+            visible: !!visible
+        }).save();
 
-    new Quote(newQuote)
-        .save()
-        .then(quote => {
-            req.flash('success', 'Quote added succesfully!');
-            res.redirect('/quotes/private',);
-        })
-        .catch(err => {
-            console.log(err);
-            req.flash('error', 'Something went wrong');
-            res.redirect('/quotes/add');
-        })
+        req.flash('success', 'Quote added succesfully!');
+        res.redirect('/quotes/private');
+    }
+    catch (err) {
+        console.log(err);
+
+        req.flash('error', 'Something went wrong');
+        res.redirect('/quotes/add');
+    }
 });
 
-router.get('/edit/:id', ensureAutheticated, (req, res) => {
-    Quote.findById(req.params.id)
-        .then(quote => {
-            if (quote.user !== req.user.id) {
-                req.flash('error', 'Not authorized.');
-                return res.redirect('/quotes/public');
-            }
+router.get('/edit/:id', ensureAutheticated, async (req, res) => {
+    const quote = await Quote.findById(req.params.id);
 
-            res.render('quotes/edit.hbs', {quote});
-        });
+    if (quote.user !== req.user.id) {
+        req.flash('error', 'Not authorized.');
+        return res.redirect('/quotes/public');
+    }
+
+    res.render('quotes/edit.hbs', {quote});
 });
 
-router.put('/:id', ensureAutheticated, (req, res) => {
-    Quote.findById(req.params.id)
-        .then(quote => {
-            quote.author = req.body.author;
-            quote.quote = req.body.quote;
-            quote.visible = !!req.body.visible;
 
-            quote.save()
-                .then(() => {
-                    req.flash('success', 'Quote updated successfully');
-                    res.redirect('/quotes/private');
-                })
-                .catch(err => {
-                    console.log(err);
+router.put('/:id', ensureAutheticated, async (req, res) => {
+    try {
+        const quote = await Quote.findById(req.params.id);
 
-                    req.flash('error', 'Failed at updating the quote.');
-                    res.redirect(`/quotes/edit/${req.params.id}`);
-                });
-        })
-        .catch(err => {
-            console.log(err);
+        quote.author = req.body.author;
+        quote.quote = req.body.quote;
+        quote.visible = !!req.body.visible;
 
-            req.flash('error', 'Failed at updating the quote.');
-            res.redirect(`/quotes/edit/${req.params.id}`);
-        });
+        await quote.save();
+
+        req.flash('success', 'Quote updated successfully');
+        res.redirect('/quotes/private');
+    }
+    catch (err) {
+        console.log(err);
+
+        req.flash('error', 'Failed at updating the quote.');
+        res.redirect(`/quotes/edit/${req.params.id}`);
+    }
 });
 
-router.post('/like/:id', (req, res) => {
-    Quote.findById(req.params.id)
-        .then(quote => {
-            quote.likes += 1;
+router.post('/like/:id', async (req, res) => {
+    try {
+        const quote = await Quote.findById(req.params.id);
+        quote.likes += 1;
 
-            quote.save()
-                .then(() => {
-                    res.back();
-                })
-                .catch(err => {
-                    console.log(err);
+        await quote.save();
+        res.back();
+    }
+    catch (err) {
+        console.log(err);
 
-                    req.flash('error', 'Failed at liking the quote.');
-                    res.back();
-                });
-        })
-        .catch(err => {
-            console.log(err);
-
-            req.flash('error', 'Failed at liking the quote.');
-            res.back();
-        });
+        req.flash('error', 'Failed at liking the quote.');
+        res.back();
+    }
 });
 
-router.delete('/:id', ensureAutheticated, (req, res) => {
-    Quote.findById(req.params.id)
-        .then(quote => {
-            quote.delete()
-                .then(() => {
-                    req.flash('success', 'Deleted successfully.');
-                    res.redirect('/quotes/private');
-                })
-                .catch(err => {
-                    console.log(err);
+router.delete('/:id', ensureAutheticated, async (req, res) => {
+    try {
+        const quote = await Quote.findById(req.params.id);
+        await quote.delete();
 
-                    req.flash('error', 'Failed at deleting the quote.');
-                    res.back();
-                });
-        })
-        .catch(err => {
-            console.log(err);
+        req.flash('success', 'Deleted successfully.');
+        res.redirect('/quotes/private');
+    }
+    catch (err) {
+        console.log(err);
 
-            req.flash('error', 'Failed at deleting the quote.');
-            res.back();
-        });
+        req.flash('error', 'Failed at deleting the quote.');
+        res.back();
+    }
 });
 
 module.exports = router;
